e75abe39a838697187603c40533b987628b6310a
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-08-20 00:01:42 +0530

    changes made

--- src/components/HomeComp1/Item.tsx
+++ src/components/HomeComp1/Item.tsx
@@ -1,8 +1,23 @@
-import styles from "components/HomeComp1/Item.module.css"
-const Item = ({content, logo}: {content: string, logo: string}) => {
+import { useNavigate } from "react-router-dom";
+import styles from "components/HomeComp1/Item.module.css";
+const Item = ({ content, logo }: { content: string; logo: string }) => {
+  const navigate = useNavigate();
   return (
-    <div className={styles.container}>{content} <img src={logo} alt="" /></div>
-  )
-}
+    <div
+      onClick={() => {
+        if (content === "BUZZ- BEE") {
+          navigate("verticals/buzzebee");
+        } else if (content === "IT SERVICES & SOLUTIONS") {
+          navigate("verticals/itservice");
+        } else {
+          navigate("verticals/cyber");
+        }
+      }}
+      className={styles.container}
+    >
+      {content} <img src={logo} alt="" />
+    </div>
+  );
+};
 
-export default Item
+export default Item;
